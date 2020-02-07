5078ff62f3d02a2fb3e8c6af5ffa0f663b9e19f5
teemal <taymal1987@gmail.com>
2020-02-06 19:25:28 -0800

    moved assumeRole inside new S3 instance to avoid timeout

--- src/main/index.js
+++ src/main/index.js
@@ -1,95 +1,115 @@
-import { app, BrowserWindow, ipcMain, ipcRenderer } from 'electron'
+import {
+  app,
+  BrowserWindow,
+  ipcMain,
+  ipcRenderer
+} from 'electron'
 var AWS = require("aws-sdk");
 const fs = require('fs');
+var path = require("path");
 
-// AWS.config.getCredentials(function(err) {
-//   if (err) console.log(err.stack);
-//   // credentials not loaded
-//   else {
-//     console.log("Access key:", AWS.config.credentials.accessKeyId);
-//     console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
-//     console.log("Region: ", AWS.config.region);
-//   }
-// });
 // =====================ASSUME ROLE====================================================
 AWS.config.region = 'us-east-1';
 
-var sts = new AWS.STS();
-sts.assumeRole({
-  RoleArn: 'arn:aws:iam::921245065062:role/supreme-S3',
-  RoleSessionName: 'supremeSession'
-}, function(err, data) {
-  if (err) { // an error occurred
-    console.log('Cannot assume role');
-    console.log(err, err.stack);
-  } else { // successful response
-    AWS.config.update({
-      accessKeyId: data.Credentials.AccessKeyId,
-      secretAccessKey: data.Credentials.SecretAccessKey,
-      sessionToken: data.Credentials.SessionToken
-    });
-  }
-});
+// var sts = new AWS.STS();
+// sts.assumeRole({
+//   RoleArn: 'arn:aws:iam::921245065062:role/supreme-S3',
+//   RoleSessionName: 'supremeSession'
+// }, function(err, data) {
+//   if (err) { // an error occurred
+//     console.log('Cannot assume role');
+//     console.log(err, err.stack);
+//   } else { // successful response
+//     AWS.config.update({
+//       accessKeyId: data.Credentials.AccessKeyId,
+//       secretAccessKey: data.Credentials.SecretAccessKey,
+//       sessionToken: data.Credentials.SessionToken
+//     });
+//   }
+// });
 
 // ==================S3============================================================
 const BUCKET_NAME = "thicc-boi-thiccbucket-1lqm0m2iu7gji"
-const s3 = new AWS.S3({
-  accessKeyId: AWS.config.credentials.accessKeyId,
-  secretAccessKey: AWS.config.credentials.secretAccessKey
-});
+// const s3 = new AWS.S3({
+//   accessKeyId: AWS.config.credentials.accessKeyId,
+//   secretAccessKey: AWS.config.credentials.secretAccessKey
+// });
 
-// const uploadFile = (fileLocation, fileName) => {
-//   // Read content from the file
-//   const fileContent = fs.readFileSync(fileLocation);
+// ================UPLOAD DIR======================================================
 
-//   // Setting up S3 upload parameters
-//   const params = {
-//       Bucket: BUCKET_NAME,
-//       Key: fileName, // File name you want to save as in S3
-//       Body: fileContent
-//   };
-
-//   // Uploading files to the bucket
-//   s3.upload(params, function(err, data) {
-//       if (err) {
-//           throw err;
-//       }
-//       console.log(`File uploaded successfully. ${data.Location}`);
-//       ipcRenderer.send('success')
-//   });
-// };
-
-var path = require("path");
-
-const uploadDir = function(s3Path, artistName, bucketName) {
-
-    let s3 = new AWS.S3();
-
-    function walkSync(currentDirPath, callback) {
-        fs.readdirSync(currentDirPath).forEach(function (name) {
-            var filePath = path.join(currentDirPath, name);
-            var stat = fs.statSync(filePath);
-            if (stat.isFile()) {
-                callback(filePath, stat);
-            } else if (stat.isDirectory()) {
-                walkSync(filePath, callback);
-            }
-        });
+const uploadDir = function (s3Path, artistName, bucketName) {
+  //assume role every upload
+  var sts = new AWS.STS();
+  // let s3 = new AWS.S3();
+  let s3 = new AWS.S3(
+  
+  sts.assumeRole({
+    RoleArn: 'arn:aws:iam::921245065062:role/supreme-S3',
+    RoleSessionName: 'supremeSession'
+  }, function (err, data) {
+    if (err) { // an error occurred
+      console.log('Cannot assume role');
+      console.log(err, err.stack);
+    } else { // successful response
+      AWS.config.update({
+        accessKeyId: data.Credentials.AccessKeyId,
+        secretAccessKey: data.Credentials.SecretAccessKey,
+        sessionToken: data.Credentials.SessionToken
+      });
     }
+  }););
 
-    walkSync(s3Path, function(filePath, stat) {
-        let bucketPath = artistName + '/' + filePath.substring(s3Path.length + 1);
-        let params = {Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath) };
-        s3.putObject(params, function(err, data) {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log('Successfully uploaded '+ bucketPath +' to ' + bucketName);
-            }
-        });
-
+  function walkSync(currentDirPath, callback) {
+    fs.readdirSync(currentDirPath).forEach(function (name) {
+      var filePath = path.join(currentDirPath, name);
+      var stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        callback(filePath, stat);
+      } else if (stat.isDirectory()) {
+        walkSync(filePath, callback);
+      }
     });
+  }
+
+  walkSync(s3Path, function (filePath, stat) {
+    let bucketPath = artistName + '/' + filePath.substring(s3Path.length + 1);
+    let params = {
+      Bucket: bucketName,
+      Key: bucketPath,
+      Body: fs.readFileSync(filePath)
+    };
+    s3.putObject(params, function (err, data) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Successfully uploaded ' + bucketPath + ' to ' + bucketName);
+      }
+    });
+
+  });
 };
+
+// ======================READ FROM S3====================================================
+var getParams = {
+  Bucket: BUCKET_NAME, //bucket name
+}
+
+// s3.getObject(getParams, function (err, data) {
+
+//   if (err) {
+//       console.log(err);
+//   } else {
+//       console.log(data.Body.toString()); //this will log data to console
+//   }
+// })
+
+// s3.listObjectsV2(getParams, function (err, data) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data.Body.toString()); //this will log data to console
+//     }
+// })
 
 
 
@@ -102,11 +122,11 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL = process.env.NODE_ENV === 'development' ?
+  `http://localhost:9080` :
+  `file://${__dirname}/index.html`
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
@@ -135,7 +155,17 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
+  s3.listObjectsV2(getParams, function (err, data) {
+    if (err) {
+      console.log(err);
+      console.log('fuck')
+    } else {
+      console.log(data.Body.toString()); //this will log data to console
+      console.log('test')
+    }
+  })
 })
+
 
 ipcMain.on('sendFiles', (event, data) => {
   // console.log(data)

dbdc2a0c30323bd6df6a15d8f94d0a576dd9d01e
teemal <taymal1987@gmail.com>
2020-02-20 16:48:05 -0800

    moved mounted method from ec2-vue project

--- src/renderer/components/CatalogPage.vue
+++ src/renderer/components/CatalogPage.vue
@@ -2,56 +2,56 @@
   <div id="wrapper">
     <div class="columns">
       <div class="column is-6 is-offset-6">
-        <input type="file" @change="getFiles" webkitdirectory directory multiple/>
+        <input type="file" @change="getFiles" webkitdirectory directory multiple />
       </div>
     </div>
     <div class="columns">
       <div class="column is-6 is-offset-6">
-        <img id="send-icon" src="@/assets/send.png" @click="uploadDir()" width="60"/>
+        <img id="send-icon" src="@/assets/send.png" @click="uploadDir()" width="60" />
       </div>
     </div>
     <div class="columns">
       <div class="column is-6 is-offset-6">
         <p>content</p>
+        <div v-for="(elm, idx) in this.data" v-bind:key="idx">
+          <ArtistCard :name="idx" :albs="elm" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import ArtistCard from "./ArtistCard";
+
 export default {
   data() {
     return {
-        fileLocation: '',
-        fileName: '',
-        artists: []
-    };
+      data: {}
+    }
   },
   methods: {
-      getFiles(event){
-          console.log(event.target.files);
-          this.fileName = event.target.files[0].name
-          this.fileLocation = event.target.files[0].path;
-      },
-    uploadDir(){
-        this.$electron.ipcRenderer.send('uploadDir', [this.fileLocation, this.fileName]);
+    getFiles(event) {
+      console.log(event.target.files);
+      this.fileName = event.target.files[0].name;
+      this.fileLocation = event.target.files[0].path;
+    },
+    uploadDir() {
+      this.$electron.ipcRenderer.send("uploadDir", [
+        this.fileLocation,
+        this.fileName
+      ]);
     }
   },
   components: {
-    Artists
+    ArtistCard
   },
-  mounted() {
-    
-  },
-  computed: {
-    ...mapGetters(["getArtists"]),
-  },
-  mounted() {
-      this.$electron.ipcRenderer.on('success', (event, data) => {
-        alert('success!');
-        // console.log(data)
-    })
+  mounted () {
+    axios
+      .get('http://ec2-54-165-246-254.compute-1.amazonaws.com:3000/entities')
+      .then((response) => {
+        this.data = response.data
+      })
   }
 };
 </script>
@@ -60,7 +60,7 @@ export default {
 .cards {
   margin-top: 20px;
 }
-#send-icon:hover{
-    cursor: pointer;
+#send-icon:hover {
+  cursor: pointer;
 }
 </style>
